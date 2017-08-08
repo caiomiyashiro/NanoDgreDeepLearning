8c9ad0f57dbe8a166a8f0cdd8adc15f1968afac4
caiomiyashiro <caiohenrique37@gmail.com>
2017-08-08 11:04:45 -0300

    First Commit

---
+++ Week 1/Brain and Body Mass Example/bmi_lifeExp.py
@@ -0,0 +1,18 @@
+# TODO: Add import statements
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv') 
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+bmi_life_model = linear_model.LinearRegression().fit(bmi_life_data[['BMI']],bmi_life_data[['Life expectancy']])
+
+# Mak a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = bmi_life_model.predict(21.07931)
+
+print('Life Expectancy Prediction = ' + str(laos_life_exp))
